7a69745d4d7f82400298cae365de06bf485a1cc6
Ryan Hennings <r.hennings11@gmail.com>
2017-01-18 23:14:16 -0800

    Iterating over dictionary lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -9,54 +9,71 @@
 import Foundation
 
 class DictionaryIteration {
+    // Q1
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
-    // Replace this comment with your answer to Question #1.
-
-    
-    
-    
-    // Question #2
-    
-    
-    
-    
-    
-    // Question #3
-   
-    
-    
-    
-    // Question #4
-    func nirvanaBestSeller() -> String {
-        let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+    // Q2
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
     }
     
-    
-    // Question #5
+    // Q3
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
    
+    // Q4
+    func nirvanaBestSeller() -> String {
+        let bandName = "Nirvana"
+        if bands[bandName] != nil {
+            return "\(bandName)'s top-selling album was \(bands[bandName]!)."
+        } else {
+            return "\(bandName) not in dictionary"
+        }
+    }
     
+    // Q5
+    func bestSellers() -> String {
+        var sentence = ""
+        for (name, album) in bands {
+            sentence += "\(name)'s top-selling album was \(album).\n"
+        }
+        // Pop \n from the string
+        sentence.remove(at: sentence.index(before: sentence.endIndex))
+        return sentence
+    }
     
-    
-    
-    // Question #6
+    // Q6
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
    
+    // Q7
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    
-    // Question #8
- 
-    
-    
-    
-
+    // Q8
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        // Lowest index of alphabet is max
+        let alphabet = ["A","B","C","D","E","F"]
+        var maxIndex = 5 // Last index of alphabet
+        var topClass = 0
+        for (grade, letter) in grades {
+            if alphabet.contains(letter) {
+                // Check if this is highest grade
+                // Force unwrap OK since checked above
+                if alphabet.index(of: letter)! < maxIndex {
+                    maxIndex = alphabet.index(of: letter)!
+                    topClass = grade
+                }
+            }
+        }// End for loop
+        return topClass
+    }
 }
