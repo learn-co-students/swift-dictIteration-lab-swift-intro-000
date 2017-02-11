91a260bceafa7f4d6b8b9c0cb093d340dca7f8b6
othmansafarini <othman_safarini@live.co.uk>
2017-02-11 09:57:15 +0400

    Done

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,52 +10,88 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = ["Nirvana" : "Nevermind",
+                 "Blondie" : "Parallel Lines",
+                 "The Kinks" : "Low Budget",
+                 "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
 
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        let album = bands["Nirvana"]
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return "\(bandName)'s top-selling album was \(album)."
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence : String = ""
+        for (keys,values) in bands {
+            sentence = "\(keys)'s top-selling album was \(values)"
+        }
+        
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
-    
+    func highestGrade() {
+        let grades = [
+            7 : "D",
+            8 : "C",
+            9 : "A",
+            10 : "F"
+        ]
+        var high : String = "Z"
+        var key : Int = 7
+        
+        for (grade, letter) in grades {
+            if letter < high {
+                high = letter
+                key = grade
+            }
+        }
+        print(key)
+        
+        
+    }
     
     
 
