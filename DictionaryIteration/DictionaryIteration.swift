8591ad93dd900fa975c6e7158b0542e3d69f1d6b
g3kenny <g3_kenny@hotmail.com>
2017-02-26 20:17:30 +0800

    Commit dicInteration lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,51 +10,83 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    var bands:[String:String] = ["Nirvana":"Nevermind",
+                                 "Blondie":"Parallel Lines",
+                                 "The Kinks":"Low Budget",
+                                 "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        let albumName = bands[bandName]
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let albumName = albumName {
+            return "\(bandName)'s top-selling album was \(albumName)."
+        }
+        
+        return "Can't find \(bandName) top-selling album."
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence = ""
+        for (bandName, albumName) in bands {
+            sentence += "\(bandName)'s top-selling album was \(albumName).\n"
+        }
+        
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades: [Int:String]) -> Int {
+        var output = 0
+        var letterGradeTemp = "Z"
+        for (grade, letterGrade) in grades {
+            if letterGrade < letterGradeTemp {
+                letterGradeTemp = letterGrade
+                output = grade
+            }
+        }
+        
+        return output
+    }
     
     
     
