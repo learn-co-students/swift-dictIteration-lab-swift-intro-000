9a67571bbf51e320c612001aa78bcfebd1e68dcd
JoelH05 <j_h_olla@hotmail.com>
2017-02-25 21:13:39 -0500

    Completed iterating over a dictionary Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,51 +10,100 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands: [String: String] = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
 
     
     
     
     // Question #2
     
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     // Question #3
    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        
+        let bestAlbum = bands[bandName]
+        var finished = String()
+        if let bestAlbum = bestAlbum {
+            finished = bestAlbum
+        }
+        return "\(bandName)'s top-selling album was \(finished)."
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String {
+        var sentence = ""
+        for (band, album) in bands {
+            sentence.append("\(band)'s top-selling album was \(album).")
+        }
+        
+        return sentence
+    }
     
     
     
     // Question #6
    
     
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
- 
+    
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var bestGrade: Int?
+        var GPA: Int = -1
+        
+        func assignGradeVal(_ val: String) -> Int {
+            let gradeDict = [
+                "A": 4,
+                "B": 3,
+                "C": 2,
+                "D": 1,
+                "F": 0
+            ]
+            return gradeDict[val]!
+        }
+        
+        for (x, y) in grades {
+            if assignGradeVal(y) > GPA {
+                GPA = assignGradeVal(y)
+                bestGrade = x
+            }
+        }
+        
+        return bestGrade!
+    }
     
     
     
