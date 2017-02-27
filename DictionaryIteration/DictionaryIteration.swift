397d6cf65633d01e149c40a9027202581ea068f4
Ben <bennalle1@gmail.com>
2017-02-27 10:25:49 -0500

    passes tests

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,52 +10,78 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
-    
+    var bands: [String : String] = ["Nirvana": "Nevermind",
+                                    "Blondie" : "Parallel Lines",
+                                    "The Kinks" : "Low Budget",
+                                    "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     // Question #2
     
-    
-    
-    
+    func addPixie() {
+    bands["Pixie"] = "Doolittle"
+    }
     
     // Question #3
    
-    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        var topSellingAlbum = ""
+        for (band, album) in bands {
+            if band == bandName {
+                topSellingAlbum = "\(bandName)'s top-selling album was \(album)."
+            }
+        }
+      return topSellingAlbum
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String {
+        var sentence: String = ""
+        
+        for (key, value) in bands {
+            sentence += "\(key)'s top-selling album was \(value)."
+        }
+        return sentence
+    }
     
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
  
-    
+    func highestGrade(_ grades: Dictionary<Int, String>) -> Int {
+        var topGrade: Int = Int()
+        for (key, value) in grades {
+            if value == "A" {
+                topGrade = key
+            }
+        }
+        return topGrade
+    }
     
     
 
