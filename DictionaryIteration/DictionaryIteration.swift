9f04d5adbcf777410f1f45f4cf6ebce2fd930ea9
TQH <tqhung2012hn@gmail.com>
2017-03-03 14:33:07 +0700

    OK

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,52 +11,74 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
-    
+    var bands = ["Nirvana" : "Nevermind",
+                "Blondie" : "Parallel Lines",
+                "The Kinks" : "Low Budget",
+                "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     // Question #2
-    
-    
-    
+    func addPixie() {
+        bands.updateValue("Doolittle", forKey: "Pixie")
+    }
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return "\(bandName)'s top-selling album was \(bands[bandName]!)."
     }
     
     
     // Question #5
-   
-    
-    
-    
+    func bestSellers() -> String {
+        var sentence: String = ""
+        for (key, value) in bands {
+            sentence += "\(key)'s top-selling album was \(value).\n"
+            // "Nirvana's top-selling album was Nevermind."
+        }
+        return sentence
+    }
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(grades: [Int : String]) -> Int {
+        var highestScore = grades[7]!
+        var gradeWithHighestScore = 7
+        for (grade, score) in grades {
+            if score < highestScore {
+                highestScore = score
+                gradeWithHighestScore = grade
+            }
+        }
+        return gradeWithHighestScore
+    }
     
     
     
-
+    
 }
