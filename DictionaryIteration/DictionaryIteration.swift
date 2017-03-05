9eeba1a6494ce7f88deeb5a40d91390d453bdd1d
willsputra <suryadiputrawilliam@gmail.com>
2017-03-05 21:45:02 +0800

    Answered questions

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,52 +11,81 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
-    
+    var bands: [String : String] = [
+        "Nirvana" : "Nevermind",
+        "Blondie" : "Parallel Lines",
+        "The Kinks" : "Low Budget",
+        "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let bandAlbum = bands[bandName]
+        var nirvanaBest = "";
+        if let bandAlbum = bandAlbum {
+            nirvanaBest =  "\(bandName)'s top-selling album was \(bandAlbum)."
+        }
+    return nirvanaBest
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence: String = "";
+        for (band, album) in bands {
+            sentence = "\(band)'s top-selling album was \(album).\n\(sentence)"
+        }
+        return sentence;
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
+     
     
     
     
     // Question #8
- 
-    
-    
+    func highestGrade(_ grades: [Int : String]) -> Int {
+        var highestGrade: Int = 0
+        for (grade, letterGrade) in grades {
+            if letterGrade == "A" {
+                highestGrade = grade
+            }
+        }
+        return highestGrade
+    }
     
 
 }
