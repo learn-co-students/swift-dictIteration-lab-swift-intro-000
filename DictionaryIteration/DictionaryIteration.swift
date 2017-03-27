82df2dd6ab77d2bd91cbbcf2b294129e614a701a
Anatolie <contact@anatolie.me>
2017-03-26 21:57:53 -0400

    Answer

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,69 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    // Question #1
+    var bands: [String:String] = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band",
+    ]
 
-    
-    
-    
     // Question #2
-    
-    
-    
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     // Question #3
-   
-    
-    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let bestSeller = bands[bandName]!
+        return "\(bandName)'s top-selling album was \(bestSeller)."
     }
     
-    
     // Question #5
-   
-    
-    
-    
+    func bestSellers() -> String {
+        
+        var sentence: String = ""
+        
+        for (band, song) in bands {
+            sentence += "\n\(band)'s top-selling album was \(song)."
+        }
+        
+        return sentence
+        
+    }
     
     // Question #6
-   
-    
-    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     // Question #7
-   
-    
-    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     // Question #8
- 
-    
-    
-    
+    func highestGrade(_ grades: [Int:String]) -> Int {
+        
+        var result: Int = 0
+        
+        for (num, grade) in grades {
+            
+            if grade == "A" {
+                result = num
+            }
+            
+        }
+        
+        return result
+        
+    }
 
 }
