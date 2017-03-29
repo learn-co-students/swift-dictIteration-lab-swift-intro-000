ea28d31855ebdaa6649dcd9d879efb9d91a613f0
alexislexi <your.immortal994@gmail.com>
2017-03-29 06:09:35 +0300

    LabDone

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,18 +10,26 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = ["Nirvana" : "Nevermind",
+                 "Blondie" : "Parallel Lines",
+                 "The Kinks" : "Low Budget",
+                 "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
+    
 
     
     
     
-    // Question #2
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     
-    // Question #3
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
    
     
     
@@ -29,34 +37,67 @@ class DictionaryIteration {
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        
+        if let bandAlbum = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(bandAlbum)."
+        } else{
+        
+        return "error"
+        }
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String{
+        var sentence = ""
+        for (bandName, bandAlbum) in bands{
+            sentence += "\(bandName)'s top-selling album was \(bandAlbum)."
+        }
+        return sentence
+    }
+       
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String{
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String{
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades: [Int:String]) -> Int{
+
+        
+        var max = 0
+        var highestScore = "F"
+        
+        for (grade, score) in grades{
+            if score < highestScore {
+                highestScore = score
+                max = grade
+            }
+            }
+        return max
+        }
+    
+        
+            }
+    
+
     
     
     
 
-}
+
