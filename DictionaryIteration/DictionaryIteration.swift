2bf4987de20b8cf698d21bb640407df7bf1650c9
Ubada2000 <ubada@digicallconnect.com>
2017-04-07 15:29:42 +0200

    Complete problems

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,87 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands: [String : String] = ["Nirvana" : "Nevermind",
+                                    "Blondie" : "Parallel Lines",
+                                    "The Kinks" : "Low Budget",
+                                    "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
 
     
     
     
     // Question #2
     
-    
+    func addPixie() {
+        bands["Pixie"]="Doolittle"
+    }
     
     
     
     // Question #3
    
-    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
       
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
-    }
-    
+        //if let value = bands[bandName] {
+            
+               return "\(bandName)'s top-selling album was \(bands[bandName]!)."
+        }
+        //return "\(bandName)'s top-selling album was not there."
+
+    //}
+
+
     
     // Question #5
    
-    
-    
+
+    func bestSellers() -> String {
+    var sentence = ""
+        for (bandName, album) in bands {
+            sentence += "\(bandName)'s top-selling album was \(album)."
+        }
+        return sentence
+    }
+
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
+
  
     
-    
-    
-
+    func highestGrade(_ grades: [Int : String]) -> Int {
+        let max = "A"
+        var maxGrade = 0
+        
+        for (grade, letterScore) in grades  {
+            
+            if letterScore <= max {
+                maxGrade = grade
+            }
+        }
+       
+        return maxGrade
+    }
 }
