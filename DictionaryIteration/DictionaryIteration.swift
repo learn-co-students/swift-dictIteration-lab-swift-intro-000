2b3a8faba38c89f4dce54ff87f57ee627a7ca93b
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-04-07 17:56:38 +0300

    Dictionary iteration functions

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -9,54 +9,98 @@
 import Foundation
 
 class DictionaryIteration {
+  
+  var bands: [String: String] = [
+    "Nirvana": "Nevermind",
+    "Blondie": "Parallel Lines",
+    "The Kinks": "Low Budget",
+    "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+  ]
+  
+  
+  
+  
+  // Question #2
+  func addPixie() {
+    bands["Pixie"] = "Doolittle"
+  }
+  
+  
+  
+  
+  // Question #3
+  func removeKinks() {
+    bands["The Kinks"] = nil
+  }
+  
+  
+  
+  // Question #4
+  func nirvanaBestSeller() -> String {
+    let bandName = "Nirvana"
     
-    // Replace this comment with your answer to Question #1.
-
+    // remove this return statement and replace it with what should be returned when you complete your implementation.
+    if let bestSeller = bands[bandName] {
+      return "\(bandName)'s top-selling album was \(bestSeller)."
+    } else {
+      return "No best seller album of \(bandName) found!"
+    }
+  }
+  
+  
+  // Question #5
+  func bestSellers() -> String {
+    var sentence: String = ""
     
-    
-    
-    // Question #2
-    
-    
-    
-    
-    
-    // Question #3
-   
-    
-    
-    
-    // Question #4
-    func nirvanaBestSeller() -> String {
-        let bandName = "Nirvana"
+    for (band, album) in bands {
+      if !sentence.isEmpty {
+        sentence += "\n"
+      }
+      sentence += "\(band)'s top-selling album was \(album)."
       
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+    }
+    return sentence
+  }
+  
+  
+  
+  
+  // Question #6
+  // The type of data a dictonary returns is a Tuple
+  func typeOfDataStructure() -> String {
+    return "Tuple"
+  }
+  
+  
+  
+  
+  // Question #7
+  func typeOfDataStructureForSchool() -> String {
+    return "B"
+  }
+  
+  
+  
+  
+  // Question #8
+  
+  func highestGrade(_ grades: [Int: String]) -> Int {
+    var previousLetter: String = ""
+    var previousGrade: Int = 0
+    
+    for (grade, letter) in grades {
+      if previousLetter.isEmpty {
+        previousLetter = letter
+        previousGrade = grade
+      } else if letter < previousLetter {
+        previousLetter = letter
+        previousGrade = grade
+      }
     }
     
-    
-    // Question #5
-   
-    
-    
-    
-    
-    // Question #6
-   
-    
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    
-    // Question #8
- 
-    
-    
-    
-
+    return previousGrade
+  }
+  
+  
+  
 }
