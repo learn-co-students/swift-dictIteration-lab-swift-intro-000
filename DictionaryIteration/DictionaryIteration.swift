3abf31eb02d7d055c60f88bda1e4331926b0b709
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-04-19 23:19:24 +1200

    dictionary lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,20 +10,27 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
-    
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+    ];
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle";
+    }
     
     
     
     
     // Question #3
    
-    
+    func removeKinks () {
+        bands["The Kinks"] = nil;
+    }
     
     
     // Question #4
@@ -31,30 +38,56 @@ class DictionaryIteration {
         let bandName = "Nirvana"
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
+        if let album = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(album)."
+        }
+        
         return ""
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String {
+        var topSellers:String = "";
+        
+        for (band, album) in bands {
+            topSellers += "\(band)'s top-selling album was \(album)."
+        }
+        
+        return topSellers
+    }
     
     
     
     // Question #6
    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades:[Int:String]) -> Int {
+        
+        for (mark, grades) in grades {
+            if grades == "A" {
+                return mark
+            }
+        }
+        
+        return -1
+    }
     
     
     
