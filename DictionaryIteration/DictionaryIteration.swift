53673a80c0b37d73567f0d637acecce2d2f6cf55
Fr3qu3ntFly3r <bszamody@gmail.com>
2017-04-20 16:14:39 +0200

    “Solution”

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,18 +10,25 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    
+var bands = ["Nirvana" : "Nevermind",
+             "Blondie":"Parallel Lines",
+             "The Kinks":"Low Budget",
+             "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
-    // Question #2
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     
-    // Question #3
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
    
     
     
@@ -30,32 +37,69 @@ class DictionaryIteration {
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
       
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let album = bands[bandName] {
+        return "\(bandName)'s top-selling album was \(album)."
+        } else {
+            return "Error"
+        }
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String{
+        var sentence = ""
+        
+        for (name, album) in bands {
+            
+            sentence += "\(name)'s top-selling album was \(album). "
+            
+        }
+        
+        
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
  
-    
+    func highestGrade(_ grades: [Int:String]) -> Int {
+        var result = 1
+        var sorter: [String] = []
+        
+        for (grade, mark) in grades {
+            if sorter.isEmpty {
+                sorter.append(mark)
+                result = grade
+            } else {
+                sorter.append(mark)
+                sorter = sorter.sorted()
+                sorter.remove(at: 1)
+                if mark == sorter[0] {
+                    result = grade
+                }
+            }
+        }
+        
+        return result
+    }
     
     
 
