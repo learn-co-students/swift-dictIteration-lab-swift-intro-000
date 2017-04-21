2d18bcf52bf1d1a4c54c27264dca773e93adb061
Kristof Buts <kr.buts@gmail.com>
2017-04-21 17:13:16 +0200

    done

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -12,50 +12,87 @@ class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
 
+    var bands: [String: String] = ["Nirvana": "Nevermind", "Blondie": "Parallel Lines", "The Kinks": "Low Budget", "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"]
+    
     
     
     
     // Question #2
     
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     // Question #3
    
-    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
       
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        var ret: String
+        if let album = bands[bandName] {
+            ret = "\(bandName)'s top-selling album was \(album)."
+        } else {
+            ret = "error"
+        }
+        
+        return ret
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String {
+        var sentence: String = ""
+        for (key, value) in bands {
+            var str = ""
+            str = "\(key)'s top-selling album was \(value)."
+            sentence += str
+        }
+        return sentence
+    }
     
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
  
-    
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var highest: Int = 0
+        for (grade, letter) in grades {
+            if highest == 0 {
+                highest = grade
+            } else {
+                if letter < grades[highest]! {
+                    highest = grade
+                }
+            }
+        }
+        
+        return highest
+    }
     
     
 
