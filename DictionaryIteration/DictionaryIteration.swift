c2259eba060aceabbe7caaafb101b6ba8ef6979b
Kassandra Hinojosa <nightsense.k@gmail.com>
2017-04-23 22:25:00 -0400

    Exercise complete

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,54 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    var bands: [String: String] = ["Nirvana": "Nevermind",
+                                   "Blondie": "Parallel Lines",
+                                   "The Kinks": "Low Budget",
+                                   "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"]
     
-    
-    
-    // Question #2
-    
-    
-    
-    
-    
-    // Question #3
-   
-    
-    
-    
-    // Question #4
-    func nirvanaBestSeller() -> String {
-        let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
     }
     
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
+
+    func nirvanaBestSeller() -> String {
+        let bandName = "Nirvana"
+        let album = bands[bandName]
+        return "\(bandName)'s top-selling album was \(album!)."
+    }
     
-    // Question #5
-   
+    func bestSellers() -> String {
+        var sentence = ""
+        for (bandName, album) in bands {
+            sentence.append("\(bandName)'s top-selling album was \(album).\n")
+        }
+        return sentence
+    }
     
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
-    
-    
-    // Question #6
-   
-    
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    
-    // Question #8
- 
-    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
+
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var grade = "F"
+        var numberStudents = 0
+        
+        for (amount, letter) in grades {
+            if letter < grade {
+                grade = letter
+                numberStudents = amount
+            }
+        }
+        
+        return numberStudents
+    }
     
 
 }
