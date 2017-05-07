9edfaf5773511a0b39556b649420f44266a340d0
Maneesh <maneesh.yadav@ahold.com>
2017-05-07 20:29:50 +0200

    Iterating Over a Dictionary Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,52 +10,85 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands : [String : String] = ["Nirvana" : "Nevermind",
+        "Blondie" : "Parallel Lines",
+        "The Kinks" : "Low Budget",
+        "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
 
-    
-    
     
     // Question #2
     
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     // Question #3
    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        
+        if let song = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(song)."
+        }
+        else
+        {
+            return ""
+        }
+        
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence : String = ""
+        for (key , value) in bands {
+            let s = "\(key)'s top-selling album was \(value).\n"
+            sentence.append(s)
+        }
+        return sentence
+    }
     
     
     
     
     // Question #6
    
-    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
  
-    
+    func highestGrade(_ grd: [Int:String]) -> Int {
+        
+        var g : Int = 0
+        
+        for (key , value) in grd {
+            if value == "A" {
+                g = key
+            }
+        }
+        
+        return g
+    }
     
     
 
