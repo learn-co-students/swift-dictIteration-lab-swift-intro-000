ec1cbeee39f45fc02ce378efd5e3ce50660af6b6
Ronak Bansal <ronakb@berkeley.edu>
2017-06-03 15:49:31 +0530

    dictionary stuff

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,17 +11,22 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
+    var bands = ["Nirvana":"Nevermind", "Blondie":"Parallel Lines","The Kinks":"Low Budget","The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
    
     
     
@@ -31,30 +36,52 @@ class DictionaryIteration {
         let bandName = "Nirvana"
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
-    }
+        if let a = bands["Nirvana"]{
+            return "\(bandName)'s top-selling album was \(a)."
+        }
+         return ""    }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence = ""
+        for (band, album) in bands {
+            sentence = sentence + "\(band)'s top-selling album was \(album)."
+        }
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades:[Int:String]) -> Int {
+        var higestGrade = "z"
+        var val = 0
+        for (x,y) in grades {
+            if y < higestGrade {
+                higestGrade = y
+                val = x
+            }
+        }
+        return val
+    }
     
     
     
