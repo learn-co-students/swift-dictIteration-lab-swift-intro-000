3d906b2788d47f6c88e848ddc2a961f3e944c6ab
tamarkandathi <tamar.kandathi@gmail.com>
2017-06-03 12:09:57 -0400

    dictionaryIteration -done

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,57 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands: [String: String] = ["Nirvana": "Nevermind",
+                                   "Blondie": "Parallel Lines",
+                                 "The Kinks": "Low Budget",
+                               "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+        ]
 
-    
-    
-    
     // Question #2
-    
-    
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     // Question #3
-   
-    
-    
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let bestSeller = bands[bandName] ?? "Unknown"
+        return "\(bandName)'s top-selling album was \(bestSeller)."
     }
     
-    
     // Question #5
-   
-    
-    
-    
-    
+    func bestSellers() -> String {
+        var sentence = ""
+        for (bandName, album) in bands {
+            sentence.append("\(bandName)'s top-selling album was \(album).")
+            print(sentence)
+        }
+        return sentence
+    }
     // Question #6
-   
-    
-    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     // Question #7
-   
-    
-    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     // Question #8
- 
-    
-    
-    
-
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var highestGrade = 0
+        for (key, value) in grades {
+            if value == "A" {
+                highestGrade = key
+            }
+        }
+        return highestGrade
+    }
 }
