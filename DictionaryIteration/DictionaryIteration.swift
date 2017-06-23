fe59a6aa6391135888e7de190633ac7b1f7a9f2b
iBrie <ibrigitte@me.com>
2017-06-23 17:06:53 +0200

    anotherone

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,19 +10,28 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+  var bands = [
+    "Nirvana" : "Nevermind",
+    "Blondie" : "Parallel Lines",
+    "The Kinks" : "Low Budget",
+    "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"
+  ]
 
-    
-    
-    
+  
+  
     // Question #2
-    
-    
+  
+  func addPixie()  {
+    bands["Pixie"] = "Doolittle"
+  }
     
     
     
     // Question #3
-   
+  func removeKinks()  {
+    bands["The Kinks"] = nil
+  
+  }
     
     
     
@@ -30,33 +39,62 @@ class DictionaryIteration {
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
       
+      
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return "\(bandName)'s top-selling album was \(bands[bandName]!)."
     }
     
-    
+  
     // Question #5
    
-    
-    
+  func bestSellers() -> String {
+    var sentence = ""
+    for (bandName , album)  in bands{
+      sentence += "\(bandName)'s top-selling album was \(album)."
+    }
+    return sentence
+  }
+
+
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+      return "Tuple"
+  }
+
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+  
+  
+      return "B"
+  }
+  func highestGrade(_grades: [Int:String]) -> Int {
+    let grades = [
+    7 : "D",
+    8 : "C",
+    9 : "A",
+    10 : "F"
+  ]
+
+    var hoogste = grades[7]!
+    var klasMetHoogste = 7
     
-    
-    
-    
-    // Question #8
- 
-    
-    
+    for (grade, score) in grades{
+      if score < hoogste{
+        klasMetHoogste = grade
+        hoogste = score
+      }
+    }
+    return klasMetHoogste
     
 
+  }
+  
 }
+    
+    
