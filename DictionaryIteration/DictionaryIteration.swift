db39174e4879adc00aab967e0c357d80042be1e5
davidtspf <david.tannenbaum@gmail.com>
2017-06-24 18:07:02 +0100

    Finished dictionary iteration

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,51 +10,65 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+        ]
 
-    
-    
-    
     // Question #2
-    
-    
-    
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     // Question #3
-   
-    
-    
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let album = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(album)."
+        } else {
+            return ""
+        }
     }
     
-    
     // Question #5
-   
-    
-    
-    
+    func bestSellers() -> String {
+        var sentence = ""
+        for (bandName, album) in bands {
+            sentence += "\(bandName)'s top-selling album was \(album).\n"
+        }
+        return sentence
+    }
     
     // Question #6
-   
-    
-    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     // Question #7
-   
-    
-    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     // Question #8
- 
+    func highestGrade(_ grades: [Int:String]) -> Int {
+        
+        var theHighest: (Int, String) = (0, "G")
+        
+        for (gradeLevel, gradeOnTest) in grades {
+            if gradeOnTest < theHighest.1 {
+                theHighest.1 = gradeOnTest
+                theHighest.0 = gradeLevel
+            }
+        }
+        return theHighest.0
+    }
     
     
     
