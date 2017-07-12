6354c1ecc65b5c3482b0890cc9004037ad420928
Reeechi <abenor1@unlv.nevada.edu>
2017-07-12 15:29:08 -0700

    Completed Dictionary Iteration Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,51 +11,80 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks(){
+        for(key, def) in bands {
+            if let band = bands["The Kinks"]{
+                bands["The Kinks"] = nil
+            }
+        }
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let definition = bands["Nirvana"]
+        
+        if let definition = definition {
+            return "\(bandName)'s top-selling album was \(definition)."
+        }
+        
+        return "oops doesnt work"
     }
     
     
     // Question #5
-   
-    
-    
-    
+    func bestSellers() -> String {
+        var sentence = ""
+        
+        for (key, value) in bands {
+            sentence += "\(key)'s top-selling album was \(value)."
+        }
+        
+        return sentence
+    }
     
     // Question #6
-   
-    
-    
+    func typeOfDataStructure() -> String{
+        return "Tuple"
+    }
     
     
     // Question #7
-   
-    
-    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     // Question #8
- 
-    
+    func highestGrade(grades: Dictionary<Int, String>) -> Int{
+        var temp = 0
+        
+        for (key, def) in grades {
+            if def == "A" {
+                temp = key
+            }
+        }
+        return temp
+    }
     
     
 
