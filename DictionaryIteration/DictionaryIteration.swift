c1966b0ac5d82e9e327dc3dfb781c8c692411abc
bebopjazz07 <branden.johnson@gmail.com>
2017-07-20 15:52:53 -0500

    Dictionary Iteration Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,91 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
     
-    // Question #2
+    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
-    // Question #3
-   
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let bestSeller = bands["Nirvana"]
+        var sentence = String()
+        if let bestSeller = bestSeller {
+            sentence = "\(bandName)'s top-selling album was \(bestSeller)."
+        }
+        return sentence
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        
+        var sentence = ""
+        
+        for (band, album) in bands {
+            sentence = sentence + "\(band)'s top-selling album was \(album)."
+        }
+        return sentence
+    }
+    
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
     
-    
-    
+    let grades = [
+        7 : "D",
+        8 : "C",
+        9 : "A",
+        10: "F"
+    ]
 
+    
+    func highestGrade(_: [Int : String]) -> Int {
+        var bestGrade = Int()
+        for (level, grade) in grades {
+            if grade < "B" {
+                bestGrade = level
+            }
+        }
+        return bestGrade
+    }
+    
 }
