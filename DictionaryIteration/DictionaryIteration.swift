444155d26c6a35c128c93922ffc6265d54557716
gbwest <gordon_west@msn.com>
2017-07-23 14:47:34 -0700

    Completed dictIteration Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,85 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    var bands: [String : String] = [
+        "Nirvana" : "Nevermind",
+        "Blondie" : "Parallel Lines",
+        "The Kinks" : "Low Budget",
+        "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
     
-    // Question #2
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
-    
-    
-    // Question #3
-   
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let album = bands[bandName] {
+        return "\(bandName)'s top-selling album was \(album)."
+        } else {
+            return "Nirvana has no top-selling album"
+        }
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence: String = ""
+        
+        for (band, album) in bands {
+            sentence = sentence + "\(band)'s top-selling album was \(album)."
+        }
+        
+        return sentence
+        
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
-    
-    
-    
+    func highestGrade(_ grades: Dictionary<Int, String>) -> Int {
+        var highLetterScore: String = "F"
+        var highGrade: Int = 0
+        for (grade, letterScore) in grades {
+            if letterScore < highLetterScore { //High score A is < F
+                highLetterScore = letterScore
+                highGrade = grade
+                
+            }
+            
+        }
+        
+            return highGrade
 
+    }
+    
+    
 }
