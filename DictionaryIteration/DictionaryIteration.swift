9ee5565ee73435a4f8d8fdb4fd310b174df01fb0
dgmalot <dgmalot@gmail.com>
2017-08-01 17:56:07 -0400

    Lots of fun with dictionaries.

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,51 +10,60 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = ["Nirvana": "Nevermind",
+                 "Blondie": "Parallel Lines",
+                 "The Kinks": "Low Budget",
+                 "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"]
 
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
-    
-    
-    // Question #2
-    
-    
-    
-    
-    
-    // Question #3
-   
-    
-    
-    
-    // Question #4
-    func nirvanaBestSeller() -> String {
-        let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+    func removeKinks() {
+        bands["The Kinks"] = nil
     }
     
     
-    // Question #5
+    func nirvanaBestSeller() -> String {
+        let bandName = "Nirvana"
+        var bandSentence: String
+        if let definition = bands[bandName] {
+            bandSentence = bandName + "'s top-selling album was " + definition + "."
+        } else {
+            bandSentence = "Could not find the band " + bandName
+        }
+        return bandSentence
+    }
+    
+    func bestSellers() -> String {
+        var sentence = ""
+        for (band, album) in bands {
+            sentence = sentence + band + "'s top-selling album was " + album + ".\n"
+        }
+        return sentence
+    }
+   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
+   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
    
     
-    
-    
-    
-    // Question #6
-   
-    
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    
-    // Question #8
- 
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var maxScore = "F"
+        var bestLevel = 0
+        for (level, score) in grades {
+            if score < maxScore {
+                maxScore = score
+                bestLevel = level
+            }
+        }
+        return bestLevel
+    }
+
     
     
     
