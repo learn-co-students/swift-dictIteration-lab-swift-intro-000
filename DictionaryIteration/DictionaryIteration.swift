05ed25534ac39e8fe246c6b33d356f086ec19950
Poon Shi Chun <scpoon123@gmail.com>
2017-08-02 20:26:05 +0800

    DONE

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,77 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    // Question 1
+    var bands = ["Nirvana": "Nevermind",
+                "Blondie": "Parallel Lines",
+                "The Kinks": "Low Budget",
+                "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        var sentence = ""
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let value = bands[bandName] {
+            sentence = "\(bandName)'s top-selling album was \(value)."
+        }
+       return sentence
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence:String = ""
+        for(band, album) in bands {
+            sentence += ("\(band)'s top-selling album was \(album).")
+        }
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
-    
-    
-    
-
+    func highestGrade(_ grades: [Int: String]) -> Int {
+        var result: Int = 0
+        for (key, value) in grades {
+            if value == "A" {
+                result = key
+            }
+        }
+        return result
+    }
 }
