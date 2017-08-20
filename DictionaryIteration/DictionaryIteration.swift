182883d52360fc38a1c743420929add6c21b70b7
Shmira <11msm7@queensu.ca>
2017-08-20 13:47:46 -0700

    Done it

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,18 +10,22 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands:[String:String] = ["Nirvana":"Nevermind","Blondie":"Parallel Lines","The Kinks":"Low Budget", "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
 
     
     
     
-    // Question #2
+    func addPixie(){
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     
-    // Question #3
+    func removeKinks(){
+        bands["The Kinks"] = nil
+    }
    
     
     
@@ -29,33 +33,67 @@ class DictionaryIteration {
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        let album = bands[bandName]
+        if let album = album {
+            return "\(bandName)'s top-selling album was \(album)."
+        }else {
+            return "No album"
+        }
+        
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String{
+        var sentence:String = ""
+        for (key,value) in bands{
+            sentence.append("\(key)'s top-selling album was \(value).\n")
+        }
+        return sentence
+    }
     
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() ->String{
+        return "B"
+    }
     
     
     
     // Question #8
  
-    
+    func highestGrade(grades:[Int:String]) -> Int {
+        var top:Int = 0
+        var out:Int = 0
+        var cur:Int = 0
+        for (key,value) in grades {
+            switch value{
+                case "F": cur = 0
+                case "D": cur = 1
+                case "C": cur = 2
+                case "B": cur = 3
+                case "A": cur = 4
+                default: cur = 0
+                
+            }
+            if cur > top {
+                top = cur
+                out = key
+            }
+        }
+        return out
+    }
     
     
 
