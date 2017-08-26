bd9887d617cc6e9c98521108ddcd946574f2462d
srigirishankar <srigirishankar@gmail.com>
2017-08-26 14:56:33 -0500

    dictionaries sold and no rains Harvey

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -12,17 +12,21 @@ class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
 
-    
-    
+    var bands: [String: String] = ["Nirvana" : "Nevermind", "Blondie" : "Parallel Lines", "The Kinks" : "Low Budget", "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band"]
+
     
     // Question #2
-    
+    func addPixie(){
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks(){
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
@@ -31,30 +35,58 @@ class DictionaryIteration {
         let bandName = "Nirvana"
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        if let bestSeller = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(bestSeller)."
+        } else {
+            return "This is crazy Harvey!"
+        }
     }
     
     
     // Question #5
-   
     
+    func bestSellers() -> String {
+        var sentence = ""
+        for (band, seller) in bands {
+            sentence += "\(band)'s top-selling album was \(seller)./n"
+        }
+        return sentence
+    }
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades : [Int: String]) -> Int {
+        var maxGrade: Int? = nil
+        var maxResult = "F"
+        for (numGrade, letterGrade) in grades {
+            if letterGrade < maxResult {
+                maxResult = letterGrade
+                maxGrade = numGrade
+            }
+        }
+        if let answer = maxGrade {
+            return answer
+        } else {
+            return 0
+        }
+    }
     
     
     
