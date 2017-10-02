32d58c1e63d2517a092cc7b18d7b1fd1e836b4cd
Yue Feng <strangedigital@gmail.com>
2017-10-02 14:11:42 -0400

    Done

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,50 +11,80 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
+    var bands = ["Nirvana":"Nevermind",
+    "Blondie":"Parallel Lines",
+    "The Kinks":"Low Budget",
+    "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"]="Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands["The Kinks"] = nil
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        if let album = bands[bandName] {
+            return "\(bandName)'s top-selling album was \(album)."
+        } else {
+            return " "
+        }
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
     }
     
     
     // Question #5
    
-    
+    func bestSellers()->String {
+        var sentence: String=""
+        for (bandName,albumName) in bands {
+            sentence += "The \(bandName)'s top-selling album was \(albumName).\n"
+        }
+        return sentence
+        
+    }
     
     
     
     // Question #6
    
-    
+    func typeOfDataStructure() -> String {
+        
+        return "Tuple"
+    }
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() ->String {
+        return "B"
+    }
     
     
     
     // Question #8
- 
+    func highestGrade(_ grades:[Int:String])->Int {
+        var curHighNum:Int = 0
+        for (inum,iletter) in grades {
+            if iletter == "A" {
+                curHighNum = inum
+            }
+        }
+        return curHighNum
+    }
     
     
     
