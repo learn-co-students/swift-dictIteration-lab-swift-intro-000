4f52733c0c504f2d3d241c5e0a5530a4feef35c8
keith1015 <chungagk@gmail.com>
2017-10-09 17:38:24 -0400

    Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,18 +10,26 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands: [String: String] = ["Nirvana":"Nevermind", "Blondie":"Parallel Lines", "The Kinks": "Low Budget", "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"]
 
     
     
     
     // Question #2
+    func addPixie()
+    {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     
     // Question #3
+    func removeKinks()
+    {
+        bands["The Kinks"] = nil
+    }
    
     
     
@@ -29,31 +37,68 @@ class DictionaryIteration {
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
+        
+        var a = bands[bandName]
+        
+        if var a = a
+        {
+            return "\(bandName)'s top-selling album was \(a)."
+        }
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return "\(bandName)'s top-selling album was ERROR."
     }
     
     
     // Question #5
-   
-    
-    
-    
+    func bestSellers() -> String
+    {
+        var sentence: [String] = []
+        
+        for (band, value) in bands
+        {
+            sentence.append("\(band)'s top-selling album was \(value).\n")
+        }
+        
+        return sentence.joined()
+        
+    }
     
     // Question #6
-   
-    
-    
-    
-    
+    func typeOfDataStructure() -> String
+    {
+        return "Tuple"
+    }
+
     // Question #7
-   
-    
-    
-    
-    
+    func typeOfDataStructureForSchool() -> String
+    {
+        return "B"
+    }
+
     // Question #8
+    func highestGrade(_ grades: [Int:String]) -> Int
+    {
+        
+        var grades2:[String] = []
+        
+        for (key, val) in grades
+        {
+            grades2.append(val)
+        }
+        
+        grades2 = grades2.sorted { $0 < $1 }
+        
+        for (key, val) in grades
+        {
+            if val == grades2[0]
+            {
+                return key
+            }
+        }
+        
+        return 0
+    }
  
     
     
