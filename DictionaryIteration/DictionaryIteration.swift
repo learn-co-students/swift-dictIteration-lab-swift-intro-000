9eb51639759d09d10e7a43c7b029bd25b03ee5a0
shanetreynolds6 <shanetreynolds@icloud.com>
2017-11-26 15:25:04 -0600

    Dictionary Lab

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,52 +10,90 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = [
+        "Nirvana": "Nevermind",
+        "Blondie": "Parallel Lines",
+        "The Kinks": "Low Budget",
+        "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band",
+    ]
+    
 
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        var topSeller = ""
+        if let album = bands[bandName] {
+            topSeller = "\(bandName)'s top-selling album was \(album)."
+        }
+        return topSeller
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence: String
+        sentence = ""
+        for (band, album) in bands {
+            sentence = sentence + "\(band)'s top-selling album was \(album)."
+        }
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     // Question #8
  
+    let grades = [
+        7: "D",
+        8: "C",
+        9: "A",
+        10: "F"
+    ]
     
+    func highestGrade(_: [Int:String]) -> Int {
+        var grade = 0
+                for (num, letter) in grades {
+                        if letter == "A" {
+                                grade = num
+            }
+        }
+        return grade
+    }
     
     
 
