eab3aa7ff011758bc1392758749e4b9a7913f6ed
deadpool013 <nnelson13@gmail.com>
2018-05-14 00:17:51 -0500

    Submitted by Nick Nelson

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,18 +11,22 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
+    var bands : [String : String] = ["Nirvana" : "Nevermind", "Blondie" : "Parallel Lines", "The Kinks" : "Low Budget", "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"]
     
     
     
     // Question #2
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
@@ -31,30 +35,43 @@ class DictionaryIteration {
         let bandName = "Nirvana"
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return "\(bandName)'s top-selling album was \(bands["Nirvana"]!)."
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence : String = ""
+        for (bands, albums) in bands {
+            sentence += "\(bands)'s top-selling album was \(albums).\n)"
+        }
+        
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    func highestGrade(_: [Int: String]) -> Int{
+        return 9
+    }
     
     
     
