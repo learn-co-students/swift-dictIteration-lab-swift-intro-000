eaf37c05e8e33c10b6148f157086166b0371602e
Jayymash <mashnijason@gmail.com>
2018-06-14 15:54:42 -0400

    Dictionaries done

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -10,53 +10,80 @@ import Foundation
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
+    var bands = [
+    "Nirvana": "Nevermind",
+    "Blondie": "Parallel Lines",
+    "The Kinks": "Low Budget",
+    "The Beatles": "Sgt. Pepper's Lonely Hearts Club Band",
+    ]
 
-    
-    
-    
     // Question #2
-    
-    
-    
+    func addPixie() {
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     // Question #3
-   
+    func removeKinks() {
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
     
     // Question #4
     func nirvanaBestSeller() -> String {
         let bandName = "Nirvana"
-      
+        
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return ("\(bandName)'s top-selling album was \(bands["Nirvana"]!)")
     }
     
     
     // Question #5
-   
+    func bestSellers() -> String {
+        var sentence = ""
+        
+        for names in bands {
+            sentence = ("\(names)'s top selling album was \(bands)")
+        }
+        
+        return sentence
+    }
     
     
     
     
     // Question #6
-   
+    func typeOfDataStructure() -> String {
+        return "Tuple"
+    }
     
     
     
     
     // Question #7
-   
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
     
     
     // Question #8
- 
+    let grades = [
+        7 : "D",
+        8 : "C",
+        9 : "A",
+        10 : "F"
+    ]
     
-    
-    
-
+    func highestGrade(grades: [Int:  String]) {
+        for (grade, score) in grades {
+            if score == "A" {
+                print (grade)
+            } else {
+                print ("No one got an A on the exam")
+            }
+        }
+    }
 }
