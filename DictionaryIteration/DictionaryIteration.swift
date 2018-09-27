361f9734fdfe34808009df0b4bbe37f3ed1a2f43
Gustavo Picciuto <gustavo.picciutomaury@live.com>
2018-09-26 22:10:38 -0400

    Complete problems

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -7,56 +7,81 @@
 //
 
 import Foundation
+import UIKit
 
 class DictionaryIteration {
     
-    // Replace this comment with your answer to Question #1.
-
+    //    1.
+    
+    var bands = [
+        "Nirvana":"Nevermind",
+        "Blondie":"Parallel Lines",
+        "The Kinks":"Low Budget",
+        "The Beatles":"Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
+    //    2.
     
-    // Question #2
-    
-    
-    
-    
-    
-    // Question #3
-   
-    
-    
-    
-    // Question #4
-    func nirvanaBestSeller() -> String {
-        let bandName = "Nirvana"
-      
-        // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+    func addPixie(){
+        bands["Pixie"] = "Doolittle"
     }
     
     
-    // Question #5
-   
+    //    3.
+    
+    func removeKinks(){
+        bands.removeValue(forKey: "The Kinks")
+    }
     
     
+    //    4.
+    
+    func nirvanaBestSeller() -> String {
+        var returnString = String()
+        let bandName = "Nirvana"
+        if let bandAlbum = bands["Nirvana"]{
+            returnString = "\(bandName)'s top-selling album was \(bandAlbum)."
+        }
+        return returnString
+    }
+    
+    //    5.
+    func bestSellers() -> String {
+        var sentence: String = ""
+        for (band, album) in bands {
+            sentence.append("\(band)'s top-selling album was \(album).\n")
+        }
+        return sentence
+    }
+    
+    //    //6.
+    
+    func typeOfDataStructure() -> String{
+        return "Tuple"
+    }
+    
+    //    7.
+    
+    func typeOfDataStructureForSchool() -> String {
+        return "B"
+    }
     
     
-    // Question #6
-   
-    
-    
-    
-    
-    // Question #7
-   
-    
-    
-    
-    
-    // Question #8
- 
-    
-    
-    
+    //    8.
+    let grades = [
+        7 : "D",
+        8 : "C",
+        9 : "A",
+        10 : "F"
+    ]
 
+    //    9.
+    
+    func highestGrade(grades: [Int: String]) -> Int {
+        let gradesSorted = grades.sorted(by: { $0.value < $1.value })
+        let highestGrade = gradesSorted[0]
+        return highestGrade.key
+    }
+    
 }
